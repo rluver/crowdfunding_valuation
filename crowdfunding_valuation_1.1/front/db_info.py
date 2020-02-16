9e64b52f3739ddf544b7a0cf18048c031c1084f2
rluver <55495983+rluver@users.noreply.github.com>
2020-02-16 22:44:40 +0900

    191221

--- crowdfunding_valuation_1.1/front/db_info.py
+++ crowdfunding_valuation_1.1/front/db_info.py
@@ -5,8 +5,8 @@ import psycopg2
 
 # DB import
 
-conn = psycopg2.connect(database="wadiz", 
-                        user="asiaeco_db",
+conn = psycopg2.connect(database="", 
+                        user="",
                         host="",
                         password="",
                         port="5432") 
