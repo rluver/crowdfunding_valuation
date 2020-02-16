f02fac47ece4bae4c828994302d12a0652c8a701
rluver <skyd0608@gmail.com>
2020-02-16 22:16:55 +0900

    191221

---
+++ crowdfunding_valuation_1.1/front/db_info.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+
+
+
+# DB import
+
+conn = psycopg2.connect(database="wadiz", 
+                        user="asiaeco_db",
+                        host="",
+                        password="",
+                        port="5432") 
