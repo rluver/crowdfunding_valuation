1d96ed2975aca4fe392c8fc22dcf051f597e286e
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-03-05 00:29:57 +0900

    Add recommend by TF-IDF with LSA cosine similarity

--- test/recommendation_by_using_text2vec.R
+++ test/recommendation_by_using_text2vec.R
@@ -59,3 +59,41 @@ similarity_table %>% arrange(desc(similarity)) %>% head(5) %>% select(similarity
     table_end %>% slice(similarity_table %>% arrange(desc(similarity)) %>% head(5) %>% select(index) %>% unlist()) %>% 
       select(title)
     )
+
+
+
+
+# tfidf lsa
+# create tfidf with lsa matrix
+train_tfidf_lsa = create_dtm(i_train, vectorizer) %>% 
+  fit_transform(TfIdf$new()) %>% 
+  fit_transform(LSA$new(n_topics = 100))
+
+
+# tfidf with lsa cosine distance
+distance_table_lsa = data.frame(index = seq(sim2(train_tfidf, sen_tfidf, method = 'cosine', norm = 'l2')@Dim[1])) %>% 
+  bind_cols(dist2(train_tfidf_lsa, train_tfidf_lsa, method = 'euclidean', norm = 'l2')[12072, ] %>% as.data.frame()) %>% 
+  rename(distance = '.')
+
+
+# extract
+distance_table_lsa %>% arrange(distance) %>% head(5) %>% select(distance) %>% 
+  bind_cols(
+    table_end %>% slice(distance_table_lsa %>% arrange(distance) %>% head(5) %>% select(index) %>% unlist()) %>% 
+      select(title)
+  )
+
+
+
+# tfidf with lsa cosine similarity
+similarity_table_lsa = data.frame(index = seq(sim2(train_tfidf, sen_tfidf, method = 'cosine', norm = 'l2')@Dim[1])) %>% 
+  bind_cols(sim2(train_tfidf_lsa, train_tfidf_lsa, method = 'cosine', norm = 'l2')[12072, ] %>% as.data.frame()) %>% 
+  rename(similarity = '.')
+
+
+# extract
+similarity_table_lsa %>% arrange(desc(similarity)) %>% head(5) %>% select(similarity) %>% 
+  bind_cols(
+    table_end %>% slice(similarity_table_lsa %>% arrange(desc(similarity)) %>% head(5) %>% select(index) %>% unlist()) %>% 
+      select(title)
+  )
