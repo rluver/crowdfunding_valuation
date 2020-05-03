5fbbabbee9e9634646421eb5a3b023d11b93d31f
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-05-03 23:20:22 +0900

    refactor: Edit user_function.R to add R6 class

---
+++ crowdfunding_valuation_1.3/data/user_function.R
@@ -0,0 +1,228 @@
+require("stringr")
+require("rvest")
+require("dplyr")
+require('R6')
+
+
+
+# user define function
+getStart = function(link)
+{
+  link %>% read_html() %>% 
+    html_nodes("div.wd-ui-cont p") %>% 
+    html_text() %>% 
+    str_extract("펀딩기간.+") %>% 
+    str_extract_all("\\d{4}\\.\\d{2}\\.\\d{2}", simplify = T) %>% 
+    as.vector() %>% 
+    stringi::stri_remove_empty() %>% 
+    sort() %>% 
+    head(1) %>% 
+    
+    return()
+}
+
+
+
+getEnd = function(link)
+{
+  link %>% read_html() %>% 
+    html_nodes("div.wd-ui-cont p") %>% 
+    html_text() %>% 
+    str_extract("펀딩기간.+") %>% 
+    str_extract_all("\\d{4}\\.\\d{2}\\.\\d{2}", simplify = T) %>% 
+    as.vector() %>% 
+    stringi::stri_remove_empty() %>% 
+    sort() %>% 
+    tail(1) %>% 
+    
+    return()
+}
+
+
+
+getEncore = function(link)
+{
+  link %>% read_html() %>% 
+    html_nodes(".project-state-info p span") %>% 
+    html_text() %>% 
+    unique() %>% 
+    
+    return()
+}
+
+
+
+getBrandlink = function(link){
+  
+  str_c("https://www.wadiz.kr/web/wmypage/myprofile/makinglist/",
+        link %>% read_html() %>% 
+          html_nodes(".maker-info button") %>% 
+          html_attr("onclick") %>% 
+          stringi::stri_remove_na() %>% 
+          str_extract("[\\d]+")) %>% 
+    
+    return()
+}
+
+
+
+replaceEmpty = function(string)
+{
+  if(rlang::is_empty(string) == T)
+  {
+    return(string = NA)
+  } 
+  else
+  {
+    return(string)
+  }
+}
+
+
+
+getFundinginfoUrl = function(link)
+{
+  link %>% str_replace_all("\\/(?=[\\d]+)", "/fundingInfo/") %>%
+    
+    return()
+}
+
+
+
+getName = function(link)
+{
+  index = link %>% get_fundinginfo_url() %>%  
+    read_html() %>% 
+    html_nodes("#reward-static-product-content-app") %>% 
+    html_attr("data-product-content-list") %>% 
+    str_split(",", simplify = T) %>% 
+    str_subset("itemName") %>% 
+    str_split(":", simplify = T) %>% 
+    str_replace_all('^\\"|\\"$', "") %>% 
+    str_detect("품명|모델명")
+  
+  if(index %>% sum() != 0)
+  {
+    link %>% get_fundinginfo_url() %>% read_html() %>% 
+      html_nodes("#reward-static-product-content-app") %>% 
+      html_attr("data-product-content-list") %>% 
+      str_split(",", simplify = T) %>% str_subset("itemValue") %>% 
+      str_split(":", simplify = T) %>% str_replace_all('^\\"|\\"$', "") %>% 
+      .[index] %>% 
+      str_replace_all("\\\\r\\\\n", " ") %>% 
+      str_replace_all("\\\\r|\\\\n", " ") %>% 
+      stringi::stri_remove_empty() %>% 
+      str_c(collapse = "|") %>% 
+      
+      return()
+  }
+}
+
+
+
+getData = R6Class('getData',
+  
+  public = list(
+    link = NA,
+    
+  # initialize
+  initialize = function(link)
+  {
+    self$link = link
+  },
+  
+  
+  getStart = function()
+  {
+    link %>% read_html() %>% 
+      html_nodes("div.wd-ui-cont p") %>% 
+      html_text() %>% 
+      str_extract("펀딩기간.+") %>% 
+      str_extract_all("\\d{4}\\.\\d{2}\\.\\d{2}", simplify = T) %>% 
+      as.vector() %>% 
+      stringi::stri_remove_empty() %>% 
+      sort() %>% 
+      head(1) %>% 
+      
+      return()
+  },
+  
+  
+  getEnd = function()
+  {
+    link %>% read_html() %>% 
+      html_nodes("div.wd-ui-cont p") %>% 
+      html_text() %>% 
+      str_extract("펀딩기간.+") %>% 
+      str_extract_all("\\d{4}\\.\\d{2}\\.\\d{2}", simplify = T) %>% 
+      as.vector() %>% 
+      stringi::stri_remove_empty() %>% 
+      sort() %>% 
+      tail(1) %>% 
+      
+      return()
+  },
+  
+  
+  getEncore = function()
+  {
+    link %>% read_html() %>% 
+      html_nodes(".project-state-info p span") %>% 
+      html_text() %>% 
+      unique() %>% 
+      
+      return()
+  },
+  
+  
+  getBrandlink = function()
+  {
+    str_c("https://www.wadiz.kr/web/wmypage/myprofile/makinglist/",
+          link %>% read_html() %>% 
+            html_nodes(".maker-info button") %>% 
+            html_attr("onclick") %>% 
+            stringi::stri_remove_na() %>% 
+            str_extract("[\\d]+")) %>% 
+      
+      return()
+  },
+  
+  
+  getFundinginfoUrl = function()
+  {
+    link %>% str_replace_all("\\/(?=[\\d]+)", "/fundingInfo/") %>%
+      
+      return()
+  },
+  
+  
+  getName = function()
+  {
+    index = link %>% get_fundinginfo_url() %>%  
+      read_html() %>% 
+      html_nodes("#reward-static-product-content-app") %>% 
+      html_attr("data-product-content-list") %>% 
+      str_split(",", simplify = T) %>% 
+      str_subset("itemName") %>% 
+      str_split(":", simplify = T) %>% 
+      str_replace_all('^\\"|\\"$', "") %>% 
+      str_detect("품명|모델명")
+    
+    if(index %>% sum() != 0)
+    {
+      link %>% get_fundinginfo_url() %>% read_html() %>% 
+        html_nodes("#reward-static-product-content-app") %>% 
+        html_attr("data-product-content-list") %>% 
+        str_split(",", simplify = T) %>% str_subset("itemValue") %>% 
+        str_split(":", simplify = T) %>% str_replace_all('^\\"|\\"$', "") %>% 
+        .[index] %>% 
+        str_replace_all("\\\\r\\\\n", " ") %>% 
+        str_replace_all("\\\\r|\\\\n", " ") %>% 
+        stringi::stri_remove_empty() %>% 
+        str_c(collapse = "|") %>% 
+        
+        return()
+    }
+  }
+  )
+)
